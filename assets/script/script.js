fba59a773e3fc8e7ffc04593f2be8a02485e8c18
kalyani joshi <kalyani121@gmail.com>
2021-08-19 18:47:07 -0400

    added weather card for current weather

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,38 +2,51 @@ const APIKey = "7f73e548f4acafe9c628ae333ee106f5";
 
 //Function to view weather by city name only
 
-function weatherCityName(){
-    let locationEl = document.getElementById("location-input");
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl.value}&appid=${APIKey}`;
+function weatherCityName() {
+  let locationEl = document.getElementById("location-input");
+  const current = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl.value}&appid=${APIKey}&units=imperial`;
+  const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${locationEl.value}&appid=${APIKey}&units=imperial`;
 
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);    
-            let locNameEl = document.getElementById("location-name");
-            locNameEl.textContent = "Weather for location: " + data.name;
-
-            let currDayEl = document.getElementById("current-day");
-            let weatherImgEl = document.getElementById("weather-pic");
-            //convert date to string - currDayEl.textContent = (data.dt).toDateString();
-            // weatherImgEl.addClass = data.weather.icon;
-
-            let humidityEl = document.getElementById("humidity");
-            humidityEl.textContent = "Humidity: " + data.main.humidity;
-            console.log(humidityEl);
-            
-            // let windSpeedEl = document.getElementById("wind-container");
-            // let windDetails = [data.wind].map((w) => {
-            //     return `<tr><td>${w.speed}</td><td>${w.deg}</td><td>${w.gust}</td>`;
-            // windSpeedEl.innerHTML = windDetails.join('');
-            // })
-
-        
-            console.log(windDetails);
-        })
-    .catch((err) => {
-        console.log(err);
+  const weatherIcon = 'https://openweathermap.org/img/wn/';
+  fetch(current)
+    .then((response) => {
+      return response.json();
     })
+    .then((data) => {
+      console.log(data);
+      let locNameEl = document.getElementById("location-name");
+      locNameEl.textContent = data.name;
+
+      let currDayEl = document.getElementById("current-day");
+      
+      let currentDate = new Date(data.dt * 1000);
+      currDayEl.textContent = `${
+        currentDate.getMonth() + 1
+      }\\${currentDate.getDate()}\\${currentDate.getFullYear()}`;
+      
+      let weatherImgEl = document.getElementById("weather-icon");
+      weatherImgEl.setAttribute("src",`${weatherIcon}${data.weather[0].icon}@2x.png`);
+      weatherImgEl.setAttribute("alt",`${data.weather[0].main}`);
+
+      let weatherDescEl = document.getElementById("weather-desc");
+      weatherDescEl.textContent = data.weather[0].description;
+
+      let humidityEl = document.getElementById("humidity");
+      humidityEl.textContent = "Humidity: " + data.main.humidity;
+      console.log(humidityEl);
+
+      let currentTemp = document.getElementById("temperature");
+      currentTemp.textContent = data.main.temp;
+
+      let windSpeedEl = document.getElementById("wind-speed");
+      windSpeedEl.textContent = "Wind Speed: " + data.wind.speed;
+     
+      let uvIndexEl = document.getElementById("uv-index");
+    })
+
+    
+
+    .catch((err) => {
+      console.log(err);
+    });
 }
