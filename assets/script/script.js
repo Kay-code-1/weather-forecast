6788d164b4723a6ad4fd4262d8d41b8d9f72bbce
kalyani joshi <kalyani121@gmail.com>
2021-08-21 06:31:36 -0400

    updated JS with older constructs to ES5 from ES6 standards

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,11 +9,16 @@ var longitudeEl = document.getElementById("longitude");
 function weatherCityName() {
   let locationEl = document.getElementById("location-input");
   let weatherCardEl = document.getElementById("weather-card");
-  const current = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl.value}&appid=${APIKey}&units=imperial`;
+  const current =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    locationEl.value +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
   const weatherIcon = "https://openweathermap.org/img/wn/";
 
   fetch(current)
-    .then((response) => {
+    .then(function (response) {
       if (response.status !== 200) {
         alert("City not found. Enter correct location!");
         weatherCardEl.style.visibility = "none";
@@ -21,90 +26,80 @@ function weatherCityName() {
         return response.json();
       }
     })
-    .then((data) => {
-        console.log(data);
-        
-        weatherCardEl.style.visibility = "visible";
-  
-        let locNameEl = document.getElementById("location-name");
-        locNameEl.textContent = data.name;
-  
-        let currDayEl = document.getElementById("current-day");
-  
-        let currentDate = new Date(data.dt * 1000);
-        currDayEl.textContent = `${
-          currentDate.getMonth() + 1
-        }\\${currentDate.getDate()}\\${currentDate.getFullYear()}`;
-  
-        let weatherImgEl = document.getElementById("weather-icon");
-        weatherImgEl.setAttribute(
-          "src",
-          `${weatherIcon}${data.weather[0].icon}@2x.png`
-        );
-        weatherImgEl.setAttribute("alt", `${data.weather[0].main}`);
-  
-        let weatherDescEl = document.getElementById("weather-desc");
-        weatherDescEl.textContent = data.weather[0].description;
-  
-        let humidityEl = document.getElementById("humidity");
-        humidityEl.textContent = "Humidity: " + data.main.humidity;
-        console.log(humidityEl);
-  
-        let currentTemp = document.getElementById("temperature");
-        currentTemp.textContent = data.main.temp;
-  
-        let windSpeedEl = document.getElementById("wind-speed");
-        windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + "mph";
-  
-        latitudeEl.textContent = data.coord.lat;
-        
-        longitudeEl.textContent = data.coord.lon;
-        getUVIndex();
-      })
-  
-   
-    .catch((err) => {
+    .then(function (data) {
+      console.log(data);
+
+      weatherCardEl.style.visibility = "visible";
+
+      let locNameEl = document.getElementById("location-name");
+      locNameEl.textContent = data.name;
+
+      let currDayEl = document.getElementById("current-day");
+
+      let currentDate = new Date(data.dt * 1000);
+      currDayEl.textContent =
+        (currentDate.getMonth() +
+        1) + "/"+
+        currentDate.getDate() + "/"+
+        currentDate.getFullYear();
+
+      //}\\${currentDate.getDate()}\\${currentDate.getFullYear()}`;
+
+      let weatherImgEl = document.getElementById("weather-icon");
+      weatherImgEl.setAttribute(
+        "src",
+        weatherIcon + data.weather[0].icon + "@2x.png"
+      );
+      // `${weatherIcon}${data.weather[0].icon}@2x.png`);
+
+      weatherImgEl.setAttribute("alt", data.weather[0].main);
+
+      let weatherDescEl = document.getElementById("weather-desc");
+      weatherDescEl.textContent = data.weather[0].description;
+
+      let humidityEl = document.getElementById("humidity");
+      humidityEl.textContent = "Humidity: " + data.main.humidity;
+      console.log(humidityEl);
+
+      let currentTemp = document.getElementById("temperature");
+      currentTemp.textContent = data.main.temp;
+
+      let windSpeedEl = document.getElementById("wind-speed");
+      windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + "mph";
+
+      latitudeEl.textContent = data.coord.lat;
+      longitudeEl.textContent = data.coord.lon;
+
+      getUVIndex();
+    })
+
+    .catch(function (err) {
       console.log(err);
     });
-
-    //const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${locationEl.value}&appid=${APIKey}&units=imperial`;
-
 }
 
 function getUVIndex() {
   let uvIndexEl = document.getElementById("uv-index");
   let lat = latitudeEl.textContent;
   let lon = longitudeEl.textContent;
-  const uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
+  const uvURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
 
   fetch(uvURL)
-    .then((response) => {
+    .then(function (response) {
       return response.json();
     })
-    .then((data) => {
+    .then(function (data) {
       console.log(data);
       uvIndexEl.textContent = "UV Index: " + data.current.uvi;
-      
     })
-    .catch((err) => {});
+    .catch(function (err) {
+      console.log(err);
+    });
 }
-
-// function getCoOrdinates() {
-//     let locationEl = document.getElementById("location-input");
-//     //const current = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl.value}&appid=${APIKey}&units=imperial`;
-
-//     const locTag = `http://api.positionstack.com/v1/forward?access_key=${LocAPIKey}&query=${locationEl.value}`;
-
-//     fetch(locTag)
-//     .then((response) => {
-//         return response.json();
-//     })
-
-//     .then((data) => {
-//         console.log(data);
-//         // let latitudeEl = document.getElementById("latitude");
-
-//         // let longitudeEl = document.getElementById("longitude");
-//         // longitudeEl = data.longitude
-
-//     })}
